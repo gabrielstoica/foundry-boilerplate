197ec311003f54ad29ac402184cbbf30d11c8512
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-04-04 08:28:05 +0300

    docs: update 'Counter.s.sol' documentation

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -4,11 +4,14 @@ pragma solidity ^0.8.20;
 import { BaseScript } from "./Base.s.sol";
 import "../src/Counter.sol";
 
-// Run this script by "forge script script/Counter.s.sol 1 "bar" --sig "run(uint256,string)" --rpc-url sepolia"
+/// @notice Deploy the {Counter} contract instance
+/// Run this script by "forge script script/Counter.s.sol 1 "bar" --sig "run(uint256,string)" --rpc-url sepolia"
+///
+/// Notes:
+/// - By default, the script will only simulate the contract deployment; Pass the "--broadcast" flag to broadcast it
+/// - Optionally pass the "--verify" flag to verify the contract on Etherscan
 contract CounterScript is BaseScript {
     function run(uint256 foo, string memory bar) public broadcast returns (Counter counter) {
-        // create the Counter contract instance
-        // the contract creation will be recorded by Forge
         counter = new Counter(foo, bar);
     }
 }
